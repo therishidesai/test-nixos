ddd97c914bf3504d5408c86aef7f39046426639b
Rishi Desai <rishi@saronic.com>
2023-01-20 11:53:04 -0600

    rename kernel

--- default.nix
+++ default.nix
@@ -3,5 +3,5 @@
 let
   pkgsAarch64 = if pkgs.stdenv.buildPlatform.isAarch64 then pkgs else pkgs.pkgsCross.aarch64-multiplatform;
 in rec {
-  digi-linux-5-15 = pkgsAarch64.callPackage ./kernel/digi-linux-5-15.nix {};
+  digi-linux-5-4 = pkgsAarch64.callPackage ./kernel/digi-linux-5-4.nix {};
 }
