5b87e7d13c7927bc4c4058243a89d980bd23888c
Rishi Desai <rishi@saronic.com>
2023-04-03 12:10:14 -0500

    jetpack nixos kernelPatches example

--- default.nix
+++
@@ -1,7 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-let
-  pkgsAarch64 = if pkgs.stdenv.buildPlatform.isAarch64 then pkgs else pkgs.pkgsCross.aarch64-multiplatform;
-in rec {
-  digi-linux-5-4 = pkgsAarch64.callPackage ./kernel/digi-linux-5-4.nix {};
-}
--- flake.nix
+++ flake.nix
@@ -1,9 +1,37 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    jetpack-nixos = {
+      url = "github:anduril/jetpack-nixos";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }: {
-    legacyPackages.aarch64-linux = nixpkgs.legacyPackages.aarch64-linux.callPackage ./default.nix {};
+  outputs = { self, nixpkgs, jetpack-nixos, ... }: {
+    nixosConfigurations.test-jp5 = let
+      system = "aarch64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      modules = [
+        jetpack-nixos.nixosModules.default
+        {
+          hardware.nvidia-jetpack.enable = true;
+          hardware.nvidia-jetpack.som = "orin-agx";
+          hardware.nvidia-jetpack.carrierBoard = "devkit";
+
+          boot.kernelPatches = [{
+            name = "can-modules";
+            patch = null;
+            extraConfig = ''
+              CAN_J1939 y
+            '';
+          }];
+        }
+      ];
+    };
   };
 }
